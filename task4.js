72aa2cba54aa031a42116183029f397a839549e4
KristineGH <kristina.gharibyan@mail.ru>
2020-08-07 15:11:02 +0400

    first commit

--- task4.js
+++ task4.js
@@ -1,19 +1,19 @@
 // 4. Write a JavaScript function to add "LI" to a list element "UL"
 window.onload = function add_event(){
-    let button = document.getElementById('but1')
-    button.addEventListener('click', add_list)
+    let button = document.getElementById('but1') ;
+    button.addEventListener('click', add_list) ;
 }
 
 function add_list(){
-    let input = document.getElementById('tt1')
-    let list = document.getElementById('ul1')
+    let input = document.getElementById('tt1') ;
+    let list = document.getElementById('ul1') ;
 
-    let val = `
-        <li>` + input.value + `</li>
-    `
-    input.value = ``
-    list.innerHTML = list.appendChild(val)
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
 
-    let button = document.getElementById('but1')
-    button.addEventListener('click', add_list)
+    input.value = `` ;
+    list.appendChild(li) ;
+
+    
+    button.addEventListener('click', add_list) ;
 }
